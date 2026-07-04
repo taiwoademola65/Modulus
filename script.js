4d253594b14a5f69ce0641e8fac005864ec5fd52
Taiwo Ademola Sunday <taiwoademola065@gmail.com>
2026-07-04 21:27:52 +0100

    Add initial HTML structure, JavaScript functionality, and Live Server settings

---
+++ script.js
@@ -0,0 +1,16 @@
+ const count = () => {
+      let userInput = document.getElementById('input').value;
+      let displayArea = document.getElementById('display')
+
+      for (let i = 1; i <= userInput; i++) {
+        // let show = i;
+        // console.log(i);
+        if (i % 3 == 0 && i % 5 == 0) {
+          displayArea.innerHTML += `<p class="text-success">${i} fizz buzz</p>`
+        }else if(i % 3 == 0){
+          displayArea.innerHTML += `<p class="text-primary">${i} fizz</p>`
+        }else if(i % 5 == 0){
+          displayArea.innerHTML += `<p>${i} buzz</p>`
+        }
+          }
+            }
